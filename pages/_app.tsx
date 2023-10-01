dcefef5a4fbf3981921adbbc64d3830e257fad93
andrew <1692815+aupshaw@users.noreply.github.com>
2023-10-01 17:47:31 -0500

    core needs for mantine plus some cleanup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,23 @@
 import '../styles/global.css'
 import {AppProps} from "next/app";
+
+//MANTINE STYLES
+// core styles are required for all packages
+import '@mantine/core/styles.css';
+import {createTheme, MantineProvider} from "@mantine/core";
+
+// other css files are required only if
+// you are using components from the corresponding package
+// import '@mantine/dates/styles.css';
+// import '@mantine/dropzone/styles.css';
+// import '@mantine/code-highlight/styles.css';
+// ...
+
+const theme = createTheme({});
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+      <MantineProvider theme={theme}>
+          <Component {...pageProps} />;
+      </MantineProvider>
+    );
 }
